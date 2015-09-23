477b0f7b8b71b7fe66a387de0cb4cfce13a74598
a140203x <maickthegotic@hotmail.com>
2015-09-22 21:54:35 -0300

    Upando as classes criadas

---
+++ src/com/edupsousa/sqlizer/util/QuerySetterInterface.java
@@ -0,0 +1,9 @@
+package com.edupsousa.sqlizer.util;
+
+import java.util.LinkedHashMap;
+
+public interface QuerySetterInterface {
+	
+	public String getSql(LinkedHashMap<String, String> fields);
+
+}
